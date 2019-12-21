a69e21013946569bb796a2ac5821e0ba73aabcd0
Jonas Machon <Jmach83@gmail.com>
2019-12-22 00:40:38 +0100

    End of section 12

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -9,5 +10,24 @@ namespace DatingApp.API.Dtos
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters")]
         public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime BirthOfDate { get; set; }
+        [Required]
+        public string city { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        [Required]
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto(){
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
